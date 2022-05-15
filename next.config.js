4309b63d8e7955707dc4093b98816de26a422e5e
Abdi-01 <abdialghi@gmail.com>
2022-05-15 11:27:42 +0700

    update

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 // /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
   images: {
     domains: ["d2xjmi1k71iy2m.cloudfront.net"]
   }
